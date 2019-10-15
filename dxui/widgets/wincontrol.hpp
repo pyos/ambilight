6023baf1b0060e26c35872e8e3e4b529505e3345
pyos <pyos100500@gmail.com>
2019-10-16 00:25:06 +0200

    bind window control buttons to parent window

--- dxui/widgets/wincontrol.hpp
+++ dxui/widgets/wincontrol.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 #include "button.hpp"
-#include "grid.hpp"
 #include "../window.hpp"
 
 namespace ui {
 #define DXUI_GENERATE_BUTTON(name, clickAction) \
     struct name : button {                                                              \
-        name(window& target) : button(icon) { onClick.addForever([&]{clickAction;}); }  \
+        name() : button(icon) { onClick.addForever([&]{                                 \
+            if (auto __w = parentWindow()) clickAction(*__w); }); }                     \
     protected:                                                                          \
         winapi::com_ptr<ID3D11Texture2D> getTexture(ui::dxcontext&) const override;     \
         RECT getOuter() const override;                                                 \
@@ -19,9 +19,9 @@ namespace ui {
         } icon;                                                                         \
     }
 
-    DXUI_GENERATE_BUTTON(win_close, target.close());
-    DXUI_GENERATE_BUTTON(win_maximize, target.isMaximized() ? target.show() : target.maximize());
-    DXUI_GENERATE_BUTTON(win_minimize, target.minimize());
+    DXUI_GENERATE_BUTTON(win_close, [](window& w) { w.close(); });
+    DXUI_GENERATE_BUTTON(win_maximize, [](window& w) { w.isMaximized() ? w.show() : w.maximize(); });
+    DXUI_GENERATE_BUTTON(win_minimize, [](window& w) { w.minimize(); });
 
 #undef DXUI_GENERATE_BUTTON
 }
