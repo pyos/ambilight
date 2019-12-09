6e7a0c85bd035fb77716ef6635284c6f161869bd
pyos <pyos100500@gmail.com>
2019-12-09 19:46:45 +0100

    always pick smallest possible brightness
    the old algorithm produces nonmonotonic colors...

--- serial.hpp
+++ serial.hpp
@@ -11,14 +11,13 @@ namespace {
 
         Y5B8G8R8() : Z(0xE0), B(0), G(0), R(0) {}
         Y5B8G8R8(uint16_t r, uint16_t g, uint16_t b, uint16_t /*y*/) {
+            // APA102 has a ~13 bit dynamic range thanks to its 5-bit global brightness field.
             auto m31d32 = [](uint16_t x) { return (x / 256 * 248) + (x % 256 * 31 / 32); };
-            // APA102 has an effective 13 bit dynamic range thanks to its 5-bit global brightness
-            // field. A lot of pairs in 1..32 are coprime, so picking the smallest possible
-            // brightness is not always best; others may allow for colors closer to the desired one.
             r = m31d32(r) / 8, g = m31d32(g) / 8, b = m31d32(b) / 8;
-            for (uint8_t d = 255, i = (r > b ? r > g ? r : g : b > g ? b : g) / 256 + 1; i < 32; i++)
-                if (d > r % i + g % i + b % i)
-                    d = r % i + g % i + b % i, Z = i;
+            // TODO a lot of pairs in 1..31 are coprime, so picking the smallest possible
+            //      global brightness is not always best. Non-monotonic colors are very visible,
+            //      though, and the precise meaning of the brightness is unclear, so...
+            Z = std::max(r, std::max(g, b)) / 256 + 1;
             R = r / Z, G = g / Z, B = b / Z, Z |= 0xE0;
         }
     };
